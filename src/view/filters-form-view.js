49fa0619bb94cf0aae1bba49e8583e658079e3fc
Дубровин Василий <thebestmane@outlook.com>
2024-09-19 15:46:54 +0300

    formatting

--- src/view/filters-form-view.js
+++ src/view/filters-form-view.js
@@ -1,7 +1,8 @@
 import { createElement } from '../render';
 
 function createFiltersFormTemplate() {
-  return `<form class="trip-filters" action="#" method="get">
+  return `
+  <form class="trip-filters" action="#" method="get">
   <div class="trip-filters__filter">
     <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything">
     <label class="trip-filters__filter-label" for="filter-everything">Everything</label>

158c31b13acd57edef958021bb10717042eef65c
Дубровин Василий <thebestmane@outlook.com>
2024-09-24 14:38:02 +0300

    leaves only one presenter

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import FiltersPresenter from './presenter/filters-presenter';
-import ListPresenter from './presenter/list-presenter';
-import SortingPresenter from './presenter/sorting-presenter';
+import GlobalPresenter from './presenter/globalPresenter';
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
 const listContainer = document.querySelector('.trip-events');
 const sortingContainer = document.querySelector('.trip-events');
 
-const filtersPresenter = new FiltersPresenter(filtersContainer);
-const listPresenter = new ListPresenter(listContainer);
-const sortingPresenter = new SortingPresenter(sortingContainer);
+const globalPresenter = new GlobalPresenter(
+  {
+    filtersContainer: filtersContainer,
+    listContainer: listContainer,
+    sortingContainer: sortingContainer
+  }
+);
 
-filtersPresenter.init();
-sortingPresenter.init();
-listPresenter.init();
+globalPresenter.init();
--- src/presenter/globalPresenter.js
+++ src/presenter/globalPresenter.js
@@ -2,20 +2,28 @@ import PointView from '../view/point-view';
 import ListView from '../view/list-view';
 import PointEditingView from '../view/point-editing-view';
 import PointCreatingView from '../view/point-creating-view';
+import FiltersView from '../view/filters-view';
+import SortingView from '../view/sorting-view';
 
 import { render, RenderPosition } from '../render';
 
-export default class ListPresenter {
-  constructor(listContainer) {
-    this.container = listContainer;
+
+export default class GlobalPresenter {
+  constructor({listContainer, sortingContainer, filtersContainer}) {
+    this.listContainer = listContainer;
+    this.filtersContainer = filtersContainer;
+    this.sortingContainer = sortingContainer;
   }
 
+  filtersComponent = new FiltersView;
   listComponent = new ListView;
   pointCreatingComponent = new PointView;
   pointEditingComponent = new PointView;
 
   init() {
-    render(this.listComponent, this.container);
+    render(this.filtersComponent, this.filtersContainer);
+    render(new SortingView, this.sortingContainer);
+    render(this.listComponent, this.listContainer);
 
     render(this.pointCreatingComponent, this.listComponent.getElement());
     render(new PointCreatingView, this.pointCreatingComponent.getElement(), RenderPosition.AFTERBEGIN);

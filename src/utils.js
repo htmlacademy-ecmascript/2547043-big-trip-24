48bfbd241d73ee3d936f2e29f3f9650e249226f9
Дубровин Василий <thebestmane@outlook.com>
2024-09-27 01:44:46 +0300

    Time difference function implemented

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,7 @@
 import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+
+dayjs.extend(duration);
 
 function getRandomArrayElement(items) {
   return items[Math.floor(Math.random() * items.length)];
@@ -18,8 +21,15 @@ function humanizePointDate (date, DATE_FORMAT) {
   return date ? dayjs(date).format(DATE_FORMAT) : '';
 }
 
-function getTimeDiff(/*dateFrom, dateTo*/) {
-  return 'WIP';
+function getTimeDiff(dateFrom, dateTo) {
+  const timeDiff = dayjs(dateTo).diff(dateFrom, 'm');
+  const timeDiffDuration = dayjs.duration(timeDiff, 'm');
+  switch (true) {
+    case (timeDiffDuration.$d.days > 0): return timeDiffDuration.format('DD[D] HH[H] mm[M]');
+    case (timeDiffDuration.$d.hours > 0): return timeDiffDuration.format('HH[H] mm[M]');
+    case (timeDiffDuration.$d.minutes > 0): return timeDiffDuration.format('m[M]');
+    default: return 'ERROR';
+  }
 }
 
 export { getRandomArrayElement, getRandomInteger, getRandomSentenceFromText, humanizePointDate, getTimeDiff };
